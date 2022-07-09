4b57605221e490dc0a5d882b2501038adabe82f3
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-07-09 15:09:15 -0500

    Updated server.js file

--- api/server.js
+++ api/server.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-
 const PORT = process.env.PORT || 8000;
 
 const studentRouter = require('./routes/students')
@@ -16,12 +15,20 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 mongoose.connect(DATABASE_URL,  { useNewUrlParser: true }) 
 const db = mongoose.connection;
-db.on('error', error => console.error(error));
-db.once('open', () => console.log("Database Connection established"));
+db.on('error', error => console.error(error))
+db.once('open', () => console.log("Database Connection established"))
 
-app.use(express.json());
-app.use('/students', studentRouter);
+app.use(express.json())
+app.use('/api/v1/students', studentRouter)
 
+//look in the react build folder for static build
+app.use(express.static(path.join(__dirname, '../reactjs/build')));
+
+//for any routes not defined by the api, assume it's a direct request to a client-side route 
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../reactjs/build', 'index.html'));
+})
 app.listen(PORT, () => {    
     console.log(`Server started on port ${PORT}`);
     });
