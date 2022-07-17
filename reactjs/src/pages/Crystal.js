ae248f7b0c8506a409873c087491000c3328053c
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-07-17 01:59:24 -0500

    Working on update api call.

--- reactjs/src/pages/Crystal.js
+++ reactjs/src/pages/Crystal.js
@@ -7,7 +7,13 @@ function Crystal() {
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(null)
 
+  const [values, setValues] = useState({
+    name: '',
+    color: ''
+  })
+
   const { id } = useParams();
+  const navigate = useNavigate();
 
   const API_BASE = process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000/api/v1' 
@@ -39,13 +45,79 @@ function Crystal() {
       setLoading(false)
     }
   }
+
+  const deleteCrystal = async () => {
+    try{ 
+      await fetch(`${API_BASE}/crystals/${id}`, {
+        method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+        setCrystals(data)
+        navigate('/dashboard', {replace: true})
+      })
+    } catch (error){
+      setError(error.message || 'Something went wrong') 
+    }finally{
+      setLoading(false)
+    }
+  }
+
+  const updateCrystal = async () => {
+    try{ 
+      await fetch(`${API_BASE}/crystals/${id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(values)
+      })
+            .then(res => res.json())
+            .then(data => {
+               console.log({data})
+            })
+    } catch (error){
+      setError(error.message || 'Something went wrong') 
+    }finally{
+      setLoading(false)
+    }
+  }
+
+    const handleSubmit = (Event) => {
+        Event.preventDefault();
+        updateCrystal();
+    }
+
+
+  const handleInputChanges = (Event) => {
+    Event.persist();
+    setValues((values) => ({
+      ...values,
+      [Event.target.name]: Event.target.value
+    }))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Crystal Profile </h1>
         <h5>{crystals && crystals.name}</h5>
+        <button onClick={() => deleteCrystal()}>Delete Crystal</button>
         <Link to="/">Home</Link>
         <Link to="/dashboard">Dashboard</Link>
+
+        <form onSubmit={ ( ) => handleSubmit(Event)}>
+            <label>
+                Name:
+                <input type="text" name="name" value={values.name} onChange={handleInputChanges} />
+            </label>
+            <label>
+               Color:
+                <input type="text" name="color" value={values.color} onChange={handleInputChanges} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+
       </header>
     </div>
   );
